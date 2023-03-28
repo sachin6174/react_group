8358b816b9111255269c5aaffc99b624349df763
sachin6174 <sachinmehtab@gmail.com>
2023-03-29 00:09:37 +0530

    "timeout"

--- sachin/10_closures/Clousers.js
+++ sachin/10_closures/Clousers.js
@@ -1,15 +1,16 @@
 function outer(){
-    let p=0;
 
+    let p=0;
     function inner(){
         console.log(p)
     }
-    for(let i=0;i<10;i++){
-        setTimeout(()=>{
-            inner();
-            p++
-        },i*1000)
-    }
+    // for(let i=0;i<10;i++){
+    //     setTimeout(()=>{
+    //         inner();
+    //         p++
+    //     },i*1000)
+    // }
+    return inner;
 
 }
 
---
+++ sachin/node/server.js
@@ -0,0 +1,7 @@
+const http= require("http")
+
+
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080);
