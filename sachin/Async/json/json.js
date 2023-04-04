f2040e2c4c9fdf36461f759003ad455f643a4e98
sachin6174 <sachinmehtab@gmail.com>
2023-04-04 20:00:51 +0530

    export

---
+++ sachin/Async/json/json.js
@@ -0,0 +1,15 @@
+// let sachin={
+//     fname:"sachin",
+//     lname:"kumar",
+//     getName: function(){
+
+//     }
+// }
+// let myjson=JSON.stringify(sachin)
+// console.log(JSON.parse(myjson))
+
+
+
+
+
+
