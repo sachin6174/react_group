d45a2ad6b5ba0408c39435a009ad8732eb01e92d
sachin6174 <sachinmehtab@gmail.com>
2023-04-06 04:08:48 +0530

    node-projs

--- node/1.gui_data_access/index.js
+++ node/1.gui_data_access/index.js
@@ -21,7 +21,26 @@ app.get('/', function (req, res) {
 
 app.post("/",function (req, res) {
   console.log(req.body)
-  res.send(`You submitted ${req.body.name}  ${req.body.age} ${req.body.class} `)
+  res.send(`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+    body{
+      background-color: black;
+      color:white;
+    }
+  </style>
+</head>
+<body>
+  <h1>You submitted ${req.body.name}  ${req.body.age} ${req.body.class}</h1>
+</body>
+</html> 
+`)
 })
 
 
