1dcb728913c39d42fa1ca230ae41bd711552c650
sachin6174 <sachinmehtab@gmail.com>
2023-04-05 22:47:09 +0530

    node modules deleted

---
+++ node/0.page_not_found/server.js
@@ -0,0 +1,55 @@
+const app = require("express")();
+
+/*
+const express = require("express");
+const app = express();
+*/
+// let path="/"
+
+// switch (path){
+//     case "/":
+//         app.get("/", function(req,res){
+//         //   res.sendFile(__dirname+"/index.html");
+//             res.send("welcome to /")
+//         });
+//         break;        
+//     case "/":
+//         app.get("/", function(req,res){
+//         //   res.sendFile(__dirname+"/index.html");
+//             res.send("welcome to /")
+//         });
+//         break;        
+//     case "/":
+//         app.get("/", function(req,res){
+//         //   res.sendFile(__dirname+"/index.html");
+//             res.send("welcome to /")
+//         });
+//         break;        
+// }
+
+
+
+
+app.get("/sachin", function(req,res){
+//   res.sendFile(__dirname+"/index.html");
+    res.send("welcome to sachin")
+});
+
+app.get("/chandan", function(req,res){
+//   res.sendFile(__dirname+"/index.html");
+    res.send("welcome to chandan")
+});
+
+app.get("/search", function(req,res){
+  
+    let obj=req.query
+    console.log(req.query.q)
+    
+    res.sendFile(__dirname+"/index.html");
+    //res.send(`welcome to google search here is ur ${obj.q} ${obj.f}`)
+});
+
+let port=3000;
+app.listen(port, function(){
+  console.log(`Server is running on port ${port}`);
+});
