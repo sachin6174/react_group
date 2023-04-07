e9ea25110d9dfe3a54ef2eeec666ffbd1a96d65e
chandan <chandan52chd@gmail.com>
2023-04-07 17:08:07 +0530

    certificate maded

--- chandan/node project/1.certicate_generator/server.js
+++ chandan/node project/1.certicate_generator/server.js
@@ -4,3 +4,28 @@ const app=express();
 const bodyparser=require('body-parser');
 
 app.use(bodyparser.urlencoded({extended:true}));
+
+
+app.set('view-engine','pug');
+app.set('views',__dirname+'/views');
+
+app.use(express.static('public'))
+
+
+// app.use(express.static(public));
+app.get('/', function (req, res) {
+    res.sendFile(__dirname+"/index.html")
+    
+  })
+
+
+  app.post('/',(req,res)=>{
+
+    res.render('index.pug',req.body);
+  })
+  
+
+
+app.listen(3000, function () {
+    console.log('Successfully started certificate making!!!!   !')
+  })
