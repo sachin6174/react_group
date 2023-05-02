52645f3cfcf400280063d891658d5f707fb7805c
chandan <chandan52chd@gmail.com>
2023-05-02 22:43:45 +0530

    flex-box on use-state api

--- 1.node-projects/async_await_on_api/9.use-state_api_async_await/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-
-import {AppLayoutComponent} from "./components/appLayoutComponent"
-
-let root = ReactDOM.createRoot(document.getElementById("root"))
-
-root.render(<AppLayoutComponent/>)
