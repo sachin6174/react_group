6be1dbf369507eaac27edca072c4d3091b06adeb
chandan <chandan52chd@gmail.com>
2023-05-14 23:50:13 +0530

    menu card fetching

--- react-router/src/Components/Footer.js
+++ react-router/src/Components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className=" bg-black text-white flex justify-center items-center h-10 text-xl p-1">
+    <div className=" bg-black text-white flex justify-center items-center h-10 text-xl mb-0">
       copyright 2023 @ Chandan Foods
     </div>
   );
